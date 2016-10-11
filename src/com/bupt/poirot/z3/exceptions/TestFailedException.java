82bef48a78241fa91f3bc211bb3e33e28de37ff3
hui.chen <hui.chen@hulu.com>
2016-10-11 23:43:00 +0800

    add .gitignore

--- src/com/bupt/poirot/z3/exceptions/TestFailedException.java
+++
@@ -1,8 +0,0 @@
-package com.bupt.poirot.z3.exceptions;
-
-@SuppressWarnings("serial")
-public class TestFailedException extends Exception {
-	public TestFailedException() {
-		super("Check FAILED");
-	}
-};
