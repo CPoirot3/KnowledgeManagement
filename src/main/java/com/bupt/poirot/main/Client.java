c4d12d647686d73264954890e176c5415aa2b5fa
hui.chen <hui.chen@hulu.com>
2016-10-12 00:36:19 +0800

    modify

--- src/main/java/com/bupt/poirot/main/Client.java
+++ src/main/java/com/bupt/poirot/main/Client.java
@@ -9,7 +9,6 @@ import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.Property;
 import org.apache.jena.rdf.model.Resource;
-import org.apache.jena.vocabulary.VCARD;
 
 /**
  * Created by Poirot on 2016/6/15.
