aa9403c3ec4a533257fc898bceda8260aee21ae6
CPoirot3 <cholmes373@163.com>
2016-11-09 16:22:57 +0800

    add

--- src/main/java/com/bupt/poirot/z3/parseAndDeduceOWL/Main.java
+++ src/main/java/com/bupt/poirot/z3/parseAndDeduceOWL/Main.java
@@ -1,9 +1,21 @@
 package com.bupt.poirot.z3.parseAndDeduceOWL;
 
+import java.io.File;
+import java.util.Set;
+
+import org.semanticweb.HermiT.model.DLClause;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
+		System.out.println("begin");
+		File file = new File("D:/GraduateDesign/KnowledgeManagement/src/main/java/"
+				+ "com/bupt/poirot/z3/parseAndDeduceOWL/examples/ontologies/warnSchemaTest0.xml");
+		Set<DLClause> set = ParseOWL.parseOwl(file);
+		System.out.println(set.size());
+		for (DLClause dlClause: set) {
+			System.out.println(dlClause);
+		}
 	}
 
 }
--- src/main/java/com/bupt/poirot/z3/parseAndDeduceOWL/ParseOWL.java
+++ src/main/java/com/bupt/poirot/z3/parseAndDeduceOWL/ParseOWL.java
@@ -5,35 +5,27 @@
 package com.bupt.poirot.z3.parseAndDeduceOWL;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 
 import org.semanticweb.HermiT.Configuration;
 import org.semanticweb.HermiT.Reasoner;
 import org.semanticweb.HermiT.model.DLClause;
-import org.semanticweb.HermiT.model.DLOntology;
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.OWLDataFactory;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
-
-
 
 /**
  * @author Poirot
  *
  */
 public class ParseOWL {
-	public Set<DLClause> parseOwl(File file){
+	public static Set<DLClause> parseOwl(File file){
 		Reasoner reasoner = null;
 		try {
 			OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
-			OWLDataFactory dataFactory = manager.getOWLDataFactory();
+//			OWLDataFactory dataFactory = manager.getOWLDataFactory();
 			OWLOntology ontology = manager.loadOntologyFromOntologyDocument(file);
 			reasoner = new Reasoner(new Configuration(), ontology);
 		} catch (Exception e) {
