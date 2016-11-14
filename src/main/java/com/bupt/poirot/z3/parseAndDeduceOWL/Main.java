abc6e365405df64fd6d30c4e721472a39fb4046b
hui.chen <hui.chen@hulu.com>
2016-11-14 20:32:31 +0800

    modify

--- src/main/java/com/bupt/poirot/z3/parseAndDeduceOWL/Main.java
+++ src/main/java/com/bupt/poirot/z3/parseAndDeduceOWL/Main.java
@@ -1,11 +1,69 @@
 package com.bupt.poirot.z3.parseAndDeduceOWL;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import com.microsoft.z3.BoolExpr;
+import com.microsoft.z3.Context;
+import com.microsoft.z3.Expr;
+import com.microsoft.z3.FuncDecl;
+import com.microsoft.z3.Quantifier;
+import com.microsoft.z3.Solver;
+import com.microsoft.z3.Sort;
+import com.microsoft.z3.Status;
 import org.semanticweb.HermiT.model.DLClause;
 
 public class Main {
+
+    public static Pattern pattern = Pattern.compile("(.+)\\((.+?)\\)");
+
+    public static Quantifier mkQuantifier(Context ctx, String boundString, String quantifierString, Set<String> sortSet) {
+        Map<String, Sort> map = new HashMap<>();
+        String[] strings = quantifierString.split(",");
+        for (String str : strings) {
+            Matcher matcher = pattern.matcher(str);
+            if (matcher.find()) {
+
+                String string = matcher.group(2);
+                System.out.println(string);
+
+                String[] varibles = string.split(",");
+                for (String var : varibles) {
+                    if (!map.containsKey(var)) {
+                        Sort sort = ctx.mkUninterpretedSort(var);
+                        map.put(var, sort);
+                    }
+                }
+
+                Sort[] domains = new Sort[varibles.length];
+                for (int i = 0; i < domains.length; i++) {
+                    domains[i] = map.get(varibles[i]);
+                }
+                String funcName = matcher.group(1);
+
+
+                FuncDecl funcDecl = ctx.mkFuncDecl(funcName, domains, ctx.mkBoolSort());
+
+                Expr[] exprs = new Expr[varibles.length];
+                for (int i = 0; i < exprs.length; i++) {
+                    exprs[i] = ctx.mk
+                }
+                BoolExpr boolExpr = ctx.mkApp(funcdecl
+            }
+        }
+
+        strings = boundString.split(",");
+        for (String str : strings) {
+
+        }
+        Quantifier quantifier = null;
+        return quantifier;
+    }
 
 	public static void main(String[] args) {
 		System.out.println("begin");
@@ -15,10 +73,33 @@ public class Main {
 		System.out.println(set.size());
 
 
+        Set<String> quantifiersSet = new HashSet<>();
+        Set<String> boundStringSet = new HashSet<>();
+        Set<String> sortSet = new HashSet<>();
+
+        Context context = new Context();
+        Solver solver = context.mkSolver();
+
 		for (DLClause dlClause: set) {
             String dlClauseString = dlClause.toString();
+
+			String[] strings = dlClauseString.split(":-");
+
 			System.out.println(dlClauseString);
+            String quantifier = strings[1];
+            quantifiersSet.add(quantifier);
+            String boundString = strings[0];
+            boundStringSet.add(boundString);
+            mkQuantifier(context, boundString, quantifier, sortSet);
 		}
+
+		if (solver.check() == Status.SATISFIABLE) {
+            System.out.println(Status.SATISFIABLE);
+            System.out.println(solver.getModel());
+        } else {
+            System.out.println(Status.UNSATISFIABLE);
+        }
+
 	}
 
 }
