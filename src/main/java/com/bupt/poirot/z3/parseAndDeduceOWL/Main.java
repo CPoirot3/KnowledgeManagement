0d3f031ed09f3fe95473016a673d99b17afacb4a
CPoirot3 <CPoirot3@163.com>
2016-11-14 09:37:47 +0800

    modify

--- src/main/java/com/bupt/poirot/z3/parseAndDeduceOWL/Main.java
+++ src/main/java/com/bupt/poirot/z3/parseAndDeduceOWL/Main.java
@@ -10,12 +10,14 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("begin");
 //		File file = new File("data/ontologies/warnSchemaTest0.xml");
-        File file = new File("data/Schema.owl");
+        File file = new File("data/schema.owl");
 		Set<DLClause> set = ParseOWL.parseOwl(file);
 		System.out.println(set.size());
 
+
 		for (DLClause dlClause: set) {
-			System.out.println(dlClause);
+            String dlClauseString = dlClause.toString();
+			System.out.println(dlClauseString);
 		}
 	}
 
--- src/main/java/com/bupt/poirot/z3/parseAndDeduceOWL/ParseOWL.java
+++ src/main/java/com/bupt/poirot/z3/parseAndDeduceOWL/ParseOWL.java
@@ -95,6 +95,6 @@ public class ParseOWL {
 		}
 	}
 	public static void main(String[] args){
-	
+
 	}
 }
