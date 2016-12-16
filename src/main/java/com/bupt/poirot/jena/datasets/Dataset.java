8f0ae4e7f1392f5607361b15729a0d006e86afb6
hui.chen <hui.chen@hulu.com>
2016-12-16 21:03:07 +0800

    modify

---
+++ src/main/java/com/bupt/poirot/jena/datasets/Dataset.java
@@ -0,0 +1,7 @@
+package com.bupt.poirot.jena.datasets;
+
+/**
+ * Created by hui.chen on 2016/12/16.
+ */
+public class Dataset {
+}
--- src/main/java/com/bupt/poirot/jena/datasets/HttpAction.java
+++
@@ -1,8 +0,0 @@
-package com.bupt.poirot.jena.datasets;
-
-/**
- * Created by hui.chen on 12/14/16.
- */
-public class HttpAction {
-    public FusekiLib request;
-}
