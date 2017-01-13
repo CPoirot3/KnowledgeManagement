b57fa2f903de3d2fcddcc418c0e65a4db7de0e5b
hui.chen <hui.chen@hulu.com>
2017-01-13 17:37:00 +0800

    modify

--- src/main/java/com/bupt/poirot/knowledgeBase/datasets/DatasetFactory.java
+++ src/main/java/com/bupt/poirot/knowledgeBase/datasets/DatasetFactory.java
@@ -42,18 +42,32 @@ public class DatasetFactory {
         return datasetMap.get(datasetName);
     }
 
-    public Dataset removeDatasetByName(String datasetName) {
+    public boolean removeDatasetByName(String datasetName) {
         Dataset dataset = datasetMap.remove(datasetName);
-        return dataset;
+        TDBFactory.release(dataset);
+        System.out.println("./resource" + File.separator + datasetName);
+        File dir = new File("./resource" + File.separator + datasetName);
+        System.out.println(dir.exists());
+        deleteFile(dir);
+        return true;
     }
 
+    public void deleteFile(File file) {
+        if (file.isDirectory()) {
+            for (File f : file.listFiles()) {
+                deleteFile(f);
+            }
+        }
+        file.delete();
+    }
 
     public static void main(String[] args) {
         DatasetFactory datasetFactory = new DatasetFactory();
-        datasetFactory.createDatasetByName("test");
+        datasetFactory.createDatasetByName("test2");
+        datasetFactory.removeDatasetByName("test1");
 
         for (Dataset dataset : datasetFactory.datasetMap.values()) {
-            System.out.println(dataset.getDefaultModel());
+            System.out.println(dataset.getContext());
         }
     }
 
